3a6b9374d63232baaa84e369559024b8710e4d6b
atariryuma <ryuma.atari@gmail.com>
2026-04-14 07:00:52 +0900

    chore: gitignore push helper artifacts

--- _push.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d C:\Users\atari\rika-mynote-push
-git add -A > "%TEMP%\git_out.txt" 2>&1
-git commit -m "feat: apply MkDocs Material best practices (abbr, snippets, repo edit link, refined JS/CSS)" >> "%TEMP%\git_out.txt" 2>&1
-git push origin main >> "%TEMP%\git_out.txt" 2>&1
-echo DONE >> "%TEMP%\git_out.txt"
